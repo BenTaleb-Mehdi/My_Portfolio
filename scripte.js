a1e3df89c933c617ed3430a4f947f50e262a4fb5
Mehdi_BenTaleb <mehdibentaleb548@gmail.com>
2024-12-20 19:30:36 +0100

    Add files via upload

--- scripte.js
+++ scripte.js
@@ -17,29 +17,48 @@ closeMenu.addEventListener("click", () => {
 
 const btnLightMode = document.querySelector(".btnlightmood"),
   mdlight = document.querySelector(".mdlight moon"),
-   mdlight2 = document.querySelector(".mdlight sun"),
- btnLightMode2 = document.querySelector(".btnlightmood2");
+  mdlight2 = document.querySelector(".mdlight sun"),
+  btnLightMode2 = document.querySelector(".btnlightmood2");
 const body = document.body;
 
 btnLightMode.addEventListener("click", () => {
   // Toggle the light-mode class on the body
   body.classList.toggle("mood");
-
 });
 
 btnLightMode2.addEventListener("click", () => {
   // Toggle the light-mode class on the body
   body.classList.toggle("mood");
- 
 });
-
-
 
 const btnMore = document.querySelector(".btnMore"),
   displayMore = document.querySelector(".morePr");
 
-  btnMore.addEventListener('click',()=>{
-    displayMore.classList.toggle('active');
-  })
+btnMore.addEventListener("click", () => {
+  displayMore.classList.toggle("active");
+});
+
+const formFollow = document.querySelector(".formFollow"),
+  closeFormFpllow = document.querySelector(".closeFormFpllow"),
+  btnFollow = document.querySelector(".btnFollow"),
+  btnformmessage = document.querySelector(".btnMessage"),
+  closeFormMessage = document.querySelector(".closeFormMessage"),
+  formMessage = document.querySelector(".formmessage");
+
+btnFollow.addEventListener("click", () => {
+  formFollow.classList.toggle("active");
+  
+});
+
+closeFormFpllow.addEventListener("click", () => {
+  formFollow.classList.remove("active");
+});
 
 
+btnformmessage.addEventListener("click", () => {
+  formMessage.classList.toggle("active");
+});
+
+closeFormMessage.addEventListener("click", () => {
+  formMessage.classList.remove("active");
+});
